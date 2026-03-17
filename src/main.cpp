d63fda61dd1883a12ff4de351648da667a933d3d
Lucas Schweickert <cyberfly@gmx.net>
2026-03-17 19:36:06 +0100

    improved style

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,13 @@ static uint8_t cursor = 0;
 int temp = 437;
 int time_m = 1200;
 int hold_temp = 400;
+int hold_time = 600;
 
 menu_item_t menu_items[] = {
   {"Temp.", MENU_ITEM_TYPE_VALUE},
   {"Time", MENU_ITEM_TYPE_VALUE},
-  {"Hold temp.", MENU_ITEM_TYPE_VALUE}
+  {"Hold temp.", MENU_ITEM_TYPE_VALUE},
+  {"Hold time", MENU_ITEM_TYPE_VALUE}
 };
 
 menu_screen_t main_screen = {
@@ -36,14 +38,17 @@ void drawTitle(menu_screen_t screen) {
 
 void drawItem(menu_item_t item, bool selected, uint8_t line) {
   u8g2.setFont(u8g2_font_t0_13b_tf);
-  if (selected) {
-    u8g2.drawUTF8(0, (line + 1) * FONT_HEIGHT_PX - FONT_GLYPH_LEG_PX + (line + 1) * LINE_SPACING_PX, ">");
-  }
-  u8g2.drawUTF8(8, (line + 1) * FONT_HEIGHT_PX - FONT_GLYPH_LEG_PX + (line + 1) * LINE_SPACING_PX, item.label);
+  u8g2.drawUTF8(8, (line + 1) * FONT_HEIGHT_PX - FONT_GLYPH_LEG_PX + (line + 1) * LINE_SPACING_PX - 2, item.label);
   if (item.type == MENU_ITEM_TYPE_VALUE) {
     char value_str[10];
     dtostrf(*(item.target.value_ptr)/10.0, 0, 1, value_str);
-    u8g2.drawUTF8(128 - u8g2.getUTF8Width(value_str), (line + 1) * FONT_HEIGHT_PX - FONT_GLYPH_LEG_PX + (line + 1) * LINE_SPACING_PX, value_str);
+    u8g2.drawUTF8(128 - u8g2.getUTF8Width(value_str), (line + 1) * FONT_HEIGHT_PX - FONT_GLYPH_LEG_PX + (line + 1) * LINE_SPACING_PX - 2, value_str);
+  }
+  if (selected) {
+    u8g2.setFont(u8g2_font_unifont_t_symbols);
+    u8g2.drawGlyph(0, (line + 1) * FONT_HEIGHT_PX - FONT_GLYPH_LEG_PX + (line + 1) * LINE_SPACING_PX - 1, 0x25b6);
+    u8g2.setDrawColor(2);
+    u8g2.drawBox(0, (line) * FONT_HEIGHT_PX - FONT_GLYPH_LEG_PX + (line + 1) * LINE_SPACING_PX - 1 , 128, FONT_HEIGHT_PX + LINE_SPACING_PX - 1);
   }
 }
 
@@ -58,8 +63,8 @@ void drawScreen(menu_screen_t screen, uint8_t cursor) {
     drawItem(screen.items[1], false, 2);
     drawItem(screen.items[2], false, 3);
   } else {
-    for (size_t i = cursor-1; i < cursor+2; i++) {
-      drawItem(screen.items[i], i == cursor, i+1);
+    for (int i = 0; i < 3; i++) {
+      drawItem(screen.items[cursor+i-1], i == 1, i+1);
     }
   }
 }
@@ -68,17 +73,19 @@ void setup() {
   menu_items[0].target.value_ptr = &temp;
   menu_items[1].target.value_ptr = &time_m;
   menu_items[2].target.value_ptr = &hold_temp;
+  menu_items[3].target.value_ptr = &hold_time;
+
   u8g2.setI2CAddress(0x7A);
   u8g2.begin();
 }
 
 void loop() {
   uint32_t now = millis();
-  if (now - last_draw > 500) {
+  if (now - last_draw > 1500) {
     last_draw = now;
     u8g2.clearBuffer();
     drawScreen(main_screen, cursor);
     u8g2.sendBuffer();
-    cursor = (cursor + 1) % 3;
+    cursor = (cursor + 1) % main_screen.item_count;
   }
 }
